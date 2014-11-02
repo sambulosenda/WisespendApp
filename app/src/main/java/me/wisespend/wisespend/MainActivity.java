2394b2db61f91853b7eb3f48651a6bdca03a54be
arun-rawlani <arun.rawlani@gmail.com>
2014-11-02 01:31:04 -0500

    Corrected alignment. Added a Pay button

--- app/src/main/java/me/wisespend/wisespend/MainActivity.java
+++ app/src/main/java/me/wisespend/wisespend/MainActivity.java
@@ -5,6 +5,7 @@ package me.wisespend.wisespend;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import android.util.Log;
